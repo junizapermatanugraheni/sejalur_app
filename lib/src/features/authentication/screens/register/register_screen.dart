5a29f0eb29da142910030c47881c21d27b53c958
yordan998 <bonder995@gmail.com>
2023-02-03 16:13:44 +0700

    update registrasi image profile

--- lib/src/features/authentication/screens/register/register_screen.dart
+++ lib/src/features/authentication/screens/register/register_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sijalur_app/src/features/authentication/screens/register/register_form_widget.dart';
+// import 'package:sijalur_app/src/features/authentication/screens/register/register_form_widget.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({Key? key}) : super(key: key);
@@ -22,37 +23,97 @@ class RegisterScreen extends StatelessWidget {
             },
           ),
         ),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.only(left: 15, top: 10, right: 15),
-              child: Column(
-                children: [
-                  Container(
-                    width: 130,
-                    height: 130,
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 4, color: Colors.white),
-                      boxShadow: [
-                        BoxShadow(
-                          spreadRadius: 2,
-                          blurRadius: 10,
-                          color: Colors.black.withOpacity(0.1),
-                        ),
-                      ],
-                      shape: BoxShape.circle,
-                      image: const DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage(
-                            'https://cdn.pixabay.com/photo/2016/12/19/21/36/woman-1919143_960_720.jpg'
-                        ),
+        body: Container(
+          padding: const EdgeInsets.only(left: 16, top: 25, right: 16),
+          child: GestureDetector(
+            onTap: (){
+              FocusScope.of(context).unfocus();
+            },
+            child: ListView(
+              children: [
+                Center(
+                  child: Stack(
+                    children: [
+                      Container(
+                        width: 130,
+                        height: 130,
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                width: 4,
+                                color: Theme.of(context).scaffoldBackgroundColor),
+                            boxShadow: [
+                              BoxShadow(
+                                  spreadRadius: 2,
+                                  blurRadius: 10,
+                                  color: Colors.black.withOpacity(0.1),
+                                  offset: const Offset(0, 10))
+                            ],
+                            shape: BoxShape.circle,
+                            image: const DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(
+                                  "https://images.pexels.com/photos/3307758/pexels-photo-3307758.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250",
+                                ))),
                       ),
-                    ),
+                      Positioned(
+                          bottom: 0,
+                          right: 0,
+                          child: Container(
+                            height: 40,
+                            width: 40,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                width: 4,
+                                color: Theme.of(context).scaffoldBackgroundColor,
+                              ),
+                              color: Colors.green,
+                            ),
+                            child: const Icon(
+                              Icons.edit,
+                              color: Colors.white,
+                            ),
+                          ),
+                      ),
+                    ],
                   ),
-                   const RegisterForm(),
-                ],
-              ),
+                ),
+                const RegisterForm(),
+              ],
             ),
           ),
+        ),
+        // body: SingleChildScrollView(
+        //   child: Container(
+        //     padding: const EdgeInsets.only(left: 15, top: 10, right: 15),
+        //       child: Column(
+        //         children: [
+        //           Container(
+        //             width: 130,
+        //             height: 130,
+        //             decoration: BoxDecoration(
+        //               border: Border.all(width: 4, color: Colors.white),
+        //               boxShadow: [
+        //                 BoxShadow(
+        //                   spreadRadius: 2,
+        //                   blurRadius: 10,
+        //                   color: Colors.black.withOpacity(0.1),
+        //                 ),
+        //               ],
+        //               shape: BoxShape.circle,
+        //               image: const DecorationImage(
+        //                 fit: BoxFit.cover,
+        //                 image: NetworkImage(
+        //                     'https://cdn.pixabay.com/photo/2016/12/19/21/36/woman-1919143_960_720.jpg'
+        //                 ),
+        //               ),
+        //             ),
+        //           ),
+        //            const RegisterForm(),
+        //         ],
+        //       ),
+        //     ),
+        //   ),
         ),
     );
   }
